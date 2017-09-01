e76ef1671d303049060a128bec64cabb6af7ff6e
geeksnatal <profgeeksnatal@gmail.com>
2017-09-01 14:47:34 -0300

    +Aluno

---
+++ Aluno/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1504287995,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/zombi-sheet0.png",
+		"images/nija-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
