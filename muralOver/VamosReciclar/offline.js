c0bf504b3a649af3b849747e40c75acfa82fcba3
geeksnatal <profgeeksnatal@gmail.com>
2017-11-27 11:15:18 -0300

    Teste para mural de jogos PTI Over 2017.1

---
+++ muralOver/VamosReciclar/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1511791433,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/boy-sheet0.png",
+		"images/gril-sheet0.png",
+		"images/gril-sheet1.png",
+		"images/garrafapet-sheet0.png",
+		"images/maça-sheet0.png",
+		"images/latadealiminio-sheet0.png",
+		"images/papel-sheet0.png",
+		"images/gameoverbg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
