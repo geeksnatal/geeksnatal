2388ca8ddb9eef6bbcab8a64c79959e4189c29df
amandamaria <amanda.marry@hotmail.com>
2017-12-13 12:57:15 -0300

    Projetos 2017.2
    Projetos alunos

---
+++ Mural2017-2/NaturezaRaivosa/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1513180479,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tbg.png",
+		"images/sobrevivete-sheet0.png",
+		"images/sobrevivete-sheet1.png",
+		"images/sobrevivete-sheet2.png",
+		"images/sobrevivete-sheet3.png",
+		"images/sobrevivete-sheet4.png",
+		"images/sobrevivete-sheet5.png",
+		"images/erva-sheet0.png",
+		"images/erva-sheet1.png",
+		"images/parede-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/play-sheet0.png",
+		"images/fogos-sheet0.png",
+		"images/fogos-sheet1.png",
+		"images/tiledbackground.png",
+		"images/gameover-sheet0.png",
+		"images/win-sheet0.png",
+		"images/titulospr-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ Mural2017-2/Rafael/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1513180239,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/porco-sheet0.png",
+		"images/grama-sheet0.png",
+		"images/girafa-sheet0.png",
+		"images/arbusto-sheet0.png",
+		"images/spok.png",
+		"images/btnplay-sheet0.png",
+		"images/btnplay-sheet1.png",
+		"images/porco2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
