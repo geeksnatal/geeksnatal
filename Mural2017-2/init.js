43be77279db734ff3af15929ca1b3dad8c797d96
amandamaria <amanda.marry@hotmail.com>
2017-12-20 12:29:24 -0300

    Corrigido erro
    Corrigido erro

--- Mural2017-2/init.js
+++ Mural2017-2/init.js
@@ -23,10 +23,10 @@ window.onload = function() {
 	var divConteudoJogos = document.getElementById("conteudo-jogos");
 	//divConteudoJogos.innerHTML =  
 	var i =0;
-	var html = "";
-		html = html.concat(    "<div class='col s10 m6 l3 offset-s1'>"+
+	var html = "";		
 	for (; i < listaJogos.informacoes.length; i++) {
-      html = html.concat ("<div class='card'>"+
+		html = html.concat("<div class='col s10 m6 l3 offset-s1'>"+
+		"<div class='card'>"+
         "<div class='card-image'>"+
           "<a target='_blank' href='"+listaJogos.informacoes[i].link+"'><img src='"+listaJogos.informacoes[i].imagem+"'></a>"+         
          " <a class='btn-floating halfway-fab waves-effect waves-light red' target='_blank' href='"+listaJogos.informacoes[i].link+"'><i class='material-icons'>play_arrow</i></a>"+
