1c4b8eb9c3a18d015fd832a05555c8c8455c2a64
amandamaria <amanda.marry@hotmail.com>
2017-12-20 12:13:17 -0300

    Adicionado jogo do grupo do levi
    Adicionado jogo do grupo do levi

--- Mural2017-2/init.js
+++ Mural2017-2/init.js
@@ -9,13 +9,14 @@ var listaJogos = {"informacoes": [
 	{"nome": "Arthur", "imagem":"imagens/PTI - Arthur Fase0 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/NaturezaRaivosa/"},
 	{"nome": "Rafael", "imagem":"imagens/PTI - Rafael 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/Rafael/"},
 	{"nome": "Lucas", "imagem":"imagens/PTI - Lucas Furtado 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/projeto-lucasfurtado.html"},
-	{"nome": "Davi, Matheus e Filipe", "imagem":"imagens/PTI - G2 Fase1 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/projeto-g2.html"},
-	{"nome": "Malu, Samuel, Henry e Luca", "imagem":"imagens/PTI - G1 Fase1 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/projeto-g1.html"},
+	{"nome": "Malu, Samuel, Henry e Luca", "imagem":"imagens/PTI - G2 Fase1 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/projeto-g2.html"},
+	{"nome": "Davi, Matheus e Filipe", "imagem":"imagens/PTI - G1 Fase1 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/projeto-g1.html"},
 	{"nome": "Matheus", "imagem":"imagens/PTI - Matheus Carvalho 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/JogoMatheusCarvalho"},
 	{"nome": "Maria", "imagem":"imagens/PTI - Maria Isabel 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/JogoMariaIsabel"},
 	{"nome": "Oliver", "imagem":"imagens/PTI - Oliver 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/JogoOliver"},
 	{"nome": "Pedro Aquino", "imagem":"imagens/PTI - Pedro Aquino 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/JogoPedroAquino"},
-	{"nome": "Guilherme", "imagem":"imagens/PTI - Gui Fernandes 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/JogoGuiFernandes"}
+	{"nome": "Guilherme", "imagem":"imagens/PTI - Gui Fernandes 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/JogoGuiFernandes"},
+	{"nome": "Davi, Gustavo e Levi", "imagem":"imagens/PTI - Grupo Quarta Fase2 2017.2.png", "link":"https://natalsupergeeks.github.io/Mural2017-2/jogos/TheMightyAdventuresOfASeed"}
 ]};
 
 window.onload = function() {
@@ -23,8 +24,8 @@ window.onload = function() {
 	//divConteudoJogos.innerHTML =  
 	var i =0;
 	var html = "";
-	for (; i < listaJogos.informacoes.length; i++) {
 		html = html.concat(    "<div class='col s10 m6 l3 offset-s1'>"+
+	for (; i < listaJogos.informacoes.length; i++) {
       "<div class='card'>"+
         "<div class='card-image'>"+
           "<a target='_blank' href='"+listaJogos.informacoes[i].link+"'><img src='"+listaJogos.informacoes[i].imagem+"'></a>"+         
