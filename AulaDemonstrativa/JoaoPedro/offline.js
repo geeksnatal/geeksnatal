66a62ab80013fa2078a3ec5609ee835610cb32df
geeksnatal <profgeeksnatal@gmail.com>
2018-06-29 16:35:30 -0300

    Inserindo o jogo do João Pedro (Aula demonstrativa)

---
+++ AulaDemonstrativa/JoaoPedro/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1530300243,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/dg-sheet0.png",
+		"images/piayer-sheet0.png",
+		"images/piayer-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/ini-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
