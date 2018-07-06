603845805d0028a9ec3d801618019714d331f83c
amandamaria <amanda.marry@hotmail.com>
2018-07-06 10:44:03 -0300

    Jogo time attak fase 1
    Jogo time attak - Equipe programadores do futuro

--- Mural2018-1/init.js
+++ Mural2018-1/init.js
@@ -5,6 +5,7 @@ var listaJogos = {"informacoes": [
 	{"nome": "Daniel, Henrique, Ian e Matheus", "imagem":"imagens/PTI - Porta.png", "link":"https://natalsupergeeks.github.io/Mural2018-1/jogos/porta_the_game"},
 	{"nome": "Alberto, Luis e Pedro", "imagem":"imagens/PTI - MACR.png", "link":"https://natalsupergeeks.github.io/Mural2018-1/jogos/GAME-MACR/index.html"},
 	{"nome": "Marco Antônio, Ian, Vinícius e Arthur", "imagem":"imagens/PTI - GordinhoBR123.png", "link":"https://natalsupergeeks.github.io/Mural2018-1/jogos/projeto_gordinhoBR123.html"},
+	{"nome": "Marco Antônio, Ian, Vinícius e Arthur", "imagem":"imagens/PTI - TimeAttak.png", "link":"https://natalsupergeeks.github.io/Mural2018-1/jogos/projeto_programadoresDoFuturo.html"},
 	
 ]};
 
