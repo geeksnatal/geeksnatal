6a8f5c3b9cbfa74bac88193eae53a704d1ed254b
geeksnatal <profgeeksnatal@gmail.com>
2018-07-31 14:43:48 -0300

    Aula demonstrativa 31.07

---
+++ AD_Davi/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1533057684,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/davithehedgehog-sheet0.png",
+		"images/robot-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/spawnpoint-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
---
+++ AD_Lucas/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1533057183,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/moises-sheet0.png",
+		"images/biuuu-sheet0.png",
+		"images/sp-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
