8beefcf56fa63f4b85cf7291cc37e891a424b223
geeksnatal <profgeeksnatal@gmail.com>
2018-10-18 12:58:09 -0300

    AD 18.10

---
+++ AD_Juan/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1539877132,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/enemigo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
