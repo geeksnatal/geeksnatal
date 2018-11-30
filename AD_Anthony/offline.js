d216e9176e9197c5617f5322c1d333337c65122c
geeksnatal <profgeeksnatal@gmail.com>
2018-11-30 18:02:43 -0300

    Aula Anthony

---
+++ AD_Anthony/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1543611634,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bd.png",
+		"images/player-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/bala-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
