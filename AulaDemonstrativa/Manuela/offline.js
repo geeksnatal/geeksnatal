b9aae16a0385f22cab8c30ea4fc36e26b288b5f8
amandamaria <amanda.marry@hotmail.com>
2018-12-05 14:12:07 -0300

    Adicionado jogo da Manuela
    Adicionado jogo da Manuela da aula demonstratida de 05-12-2018

---
+++ AulaDemonstrativa/Manuela/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1544028331,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
