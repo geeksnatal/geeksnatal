f2fd94bf0bbceec6ab43955d86f4b14f61882e10
geeksnatal <profgeeksnatal@gmail.com>
2018-12-19 17:03:37 -0300

    EU NÃO AGUENTO MA2IS - Projetos 2018.2

--- init.js
+++ init.js
@@ -5,7 +5,7 @@ var listaJogos = {"informacoes": [
 	{"imagem":"imagens/TCC - DAVI.png", "link":"https://natalsupergeeks.github.io/Davi/"},
 	{"imagem":"imagens/TCC - Augusto.png", "link":"https://natalsupergeeks.github.io/Augusto/"},
 	{"imagem":"imagens/tcc2018.jpg", "link":"https://natalsupergeeks.github.io/Mural2018-1/"},
-	{"imagem":"imagens/tcc2018-2.png", "link":"https://natalsupergeeks.github.io/Mural2018-2/"}
+	{"imagem":"imagens/tcc2018-2.png", "link":"https://natalsupergeeks.github.io/Mural2018-2/"},
 
 ]};
 
