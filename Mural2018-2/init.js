5cd5e010e779daf1a3d0d2d42c9d33fafd3d2f7b
amandamaria <amanda.marry@hotmail.com>
2018-12-21 17:33:05 -0300

    Ajustado tamanho dos cards
    Ajustado tamanho dos cards

--- Mural2018-2/init.js
+++ Mural2018-2/init.js
@@ -25,7 +25,7 @@ window.onload = function() {
 	var html = "";
 	for (; i < listaJogos.informacoes.length; i++) {
 		html = html.concat("<div class='col s10 m6 l3 offset-s1'>"+
-		"<div class='card'>"+
+		"<div class='card' style='min-height: 310px;'>"+
         "<div class='card-image'>"+
           "<a target='_blank' href='"+listaJogos.informacoes[i].link+"'><img src='"+listaJogos.informacoes[i].imagem+"'></a>"+         
          " <a class='btn-floating halfway-fab waves-effect waves-light red' target='_blank' href='"+listaJogos.informacoes[i].link+"'><i class='material-icons'>play_arrow</i></a>"+
