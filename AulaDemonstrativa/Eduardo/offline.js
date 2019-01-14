962b266297a8be9eb7260799ab1952a0b3952b54
amandamaria <amanda.marry@hotmail.com>
2019-01-14 15:13:22 -0300

    Adicionado jogo da aula demonstrativa
    Adicionado jogo da aula demonstrativa do Eduardo, realizada no dia 11.01.2019

---
+++ AulaDemonstrativa/Eduardo/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1547489384,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/obito-sheet0.png",
+		"images/spain-sheet0.png",
+		"images/plaer-sheet0.png",
+		"images/tiro-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm",
+		"pathfind.js"
+	]
+}
