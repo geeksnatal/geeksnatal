ff7690389bf09e992e8a373b46c05789cff18abb
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-02-12 12:40:58 -0200

    Incluida aula Demo do Lucas

---
+++ AulaDemonstrativa/Lucas/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1549982211,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/player-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/all-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
