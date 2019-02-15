b7edd832070234114a5b69c6472130824c825c45
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-02-15 17:27:24 -0200

    Aula Demo - Isabelle
    Adicionada aula da Isabelle

---
+++ AulaDemonstrativa/Isabelle/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1550258712,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/jogador-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/tiro-sheet0.png",
+		"images/parede-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
