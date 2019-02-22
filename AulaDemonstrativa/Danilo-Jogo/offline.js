c07cabffc917e942fcf30a2f9f31862391bf5963
geeksnatal <profgeeksnatal@gmail.com>
2019-02-22 09:56:16 -0300

    Aula demonstrativa 21.02 15h

---
+++ AulaDemonstrativa/Danilo-Jogo/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1550839964,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/umcarad-sheet0.png",
+		"images/umapessoa-sheet0.png",
+		"images/baladeaugodaodoce-sheet0.png",
+		"images/berçocomunitario-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
