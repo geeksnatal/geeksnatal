efe54dd9b9b091013e47cb152870cd4e5b166178
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-03-01 13:40:05 -0300

    Add Aula Demo do Eduardo

---
+++ AulaDemonstrativa/Eduardo_AD/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1551458272,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/gamer-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/wall-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
