e39ef4a50972199df3af64bc8bf8ef2ba57aa003
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-06-21 17:21:05 -0300

    Aula Demo do Luís
    21.06

---
+++ AulaDemonstrativa/Luís/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1561148082,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bgelo.png",
+		"images/sprite-sheet0.png",
+		"images/inimegi-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/parede-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
