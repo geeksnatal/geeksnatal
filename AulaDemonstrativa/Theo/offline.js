dc41f70940160397472b50ed1830933cfa67143f
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-07-11 13:20:49 -0300

    Add AulaDemo - Theo
    11.07

---
+++ AulaDemonstrativa/Theo/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1562861704,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/thebackground.png",
+		"images/player-sheet0.png",
+		"images/et-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/pinkfloydthewall-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
