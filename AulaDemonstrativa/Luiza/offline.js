5ba73ecc1342090fe72ab526937751445b7d310c
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-07-27 13:09:58 -0300

    Add AulaDemo Luiza

---
+++ AulaDemonstrativa/Luiza/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1564243664,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/platform-sheet0.png",
+		"images/player1-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/bullet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
