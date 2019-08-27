78eeb4a3bbff4c209699b5850810353b95205540
brenoowesley <profgeeksnatal@gmail.com>
2019-08-27 10:50:40 -0300

    Aula demonstrativa João Gustavo

---
+++ AulaDemonstrativa/AD_JoaoGustavo/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1566912130,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fulano-sheet0.png",
+		"images/valdiscreisom-sheet0.png",
+		"images/base_do_valdiscreisom-sheet0.png",
+		"images/tiro_do_fulano-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.ogv",
+		"logo-intro-binario.webm"
+	]
+}
