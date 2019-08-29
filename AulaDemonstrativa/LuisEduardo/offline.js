20f9d1798608c934ef537e3d6461769278bf2f82
natalsupergeeks <31547199+natalsupergeeks@users.noreply.github.com>
2019-08-29 13:34:21 -0300

    Add AulaDemo LuisEduardo
    28.08 15h

---
+++ AulaDemonstrativa/LuisEduardo/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1567096249,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/1player-sheet0.png",
+		"images/ladrao-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"logo-intro-binario.mp4",
+		"logo-intro-binario.webm"
+	]
+}
